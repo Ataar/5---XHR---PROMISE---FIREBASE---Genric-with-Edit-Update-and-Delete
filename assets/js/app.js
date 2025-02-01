d5cc2d489af3aca9a6a1c2d7016a586b750cb578
Mumtazbi <your-email@example.com>
2025-02-01 13:42:00 +0530

    Added files for XHR - Firebase feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,232 +1,133 @@
 let cl = console.log;
 
-const userForm = document.getElementById("userForm");
-const title = document.getElementById("title");
-const body = document.getElementById("body");
-const userId = document.getElementById("userId");
-const sBtn = document.getElementById("sBtn");
-const uBtn = document.getElementById("uBtn");
-const cardsContainer = document.getElementById("cardsContainer");
+const userData = document.getElementById('userData');
+const title = document.getElementById('title');
+const body = document.getElementById('body');
+const userId = document.getElementById('userId');
+const sBtn = document.getElementById('sBtn');
+const uBtn = document.getElementById('uBtn');
+const dataContainer = document.getElementById('dataContainer');
+const loader = document.getElementById('loader')
 
-let Base_Url = `https://jsonplaceholder.typicode.com`;
-let Post_Url = `${Base_Url}/posts`;
 
-const snackBar = ((msg,icon)=>{
-   Swal.fire({
-     title:msg,
-     icon:icon,
-     timer:1500
-   })
+const Base_Url = `https://xhr-firebase---get-and-p-7b428-default-rtdb.firebaseio.com`;
+const Post_Url = `${Base_Url}/posts.json`
+cl(Post_Url)
 
-})
 
-const onDelete = (ele)=>{
-  let getdel_ID = ele.closest('.card').id;
-  let getdelUrl = `${Base_Url}/posts/${getdel_ID}`;
-  cl(getdelUrl);
+let  postArr = [];
 
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!"
-  }).then((result) => {
-    if (result.isConfirmed) {
-      let xhr = new XMLHttpRequest()
-      xhr.open('DELETE',getdelUrl,true)
-      xhr.send(null)
-      xhr.onload = function()
-      {
+
+const objToarr = (obj)=>{
+
+    for(let key in obj)
+    {
+        postArr.push({...obj[key] , id : key})
+    }
+    return postArr
+    
+}
+
+const onsendObjToDB = (eve)=>{
+    eve.preventDefault()
+
+    let newObj = 
+    {
+        title:title.value,
+        body:body.value,
+        userId:userId.value,
+    }
+    cl(newObj)
+    userData.reset()
+    loader.classList.remove('d-none') 
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST',Post_Url,true)
+    xhr.send(JSON.stringify(newObj))
+    xhr.onload  = ()=> 
+    {
         if(xhr.status>=200 && xhr.status<=299)
         {
-           ele.closest(".card").remove()
-        }
-      }
-      Swal.fire({
-        title: "Deleted!",
-        text: "Your file has been deleted.",
-        icon: "success",
-        timer:1000
-      });
-    }
-  });
-
- 
-} 
-
-
-const onUpdate = ()=>{
-  let updateObj = 
-  {
-    title: title.value,
-    body: body.value,
-    userId: userId.value,
-  }
-     
-     
-     userForm.reset()
-
-  let getUpdateId = localStorage.getItem('editId')
-  let updateUrl = `${Base_Url}/posts/${getUpdateId}`
-
-  let xhr = new XMLHttpRequest()
-  xhr.open('PATCH',updateUrl,true)
-  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-  xhr.send(JSON.stringify(updateObj))
-  xhr.onload = function()
-  {
-    if(xhr.status>=200 && xhr.status<=299)
-    {
+            cl(xhr.response)
         let data = JSON.parse(xhr.response)
-        cl(data)
-
-        let cardelement = document.getElementById(data.id).children;
-        cardelement[0].innerHTML = `
-        <h3>${data.title}</h3>
-        ` 
-        cardelement[1].innerHTML = `
-           <p> ${data.body}</p>
+          createCard(newObj,data)
+        }
         
-        `
-        sBtn.classList.remove('d-none')
-        uBtn.classList.add('d-none')
-        snackBar('Updated SuccessFully','success')
+    loader.classList.add('d-none') 
+       
     }
 
-    else
-    {
-       snackBar('Please Check Your DataInfo','error')
-    }
-  }
+
 }
 
 
-
-
-
-const onEdit = (ele)=>{
-  console.log(ele);
-  let getEdit_ID = ele.closest('.card').id;
-  console.log(getEdit_ID);
-
-  localStorage.setItem('editId',getEdit_ID);
-  
-  let getEdit_Url = `${Base_Url}/posts/${getEdit_ID}`;
-  console.log(getEdit_Url);
-
-  let xhr  = new XMLHttpRequest();
-  xhr.open('GET',getEdit_Url,true);
-  xhr.send(null);
-  xhr.onload =  function () {
-     if(xhr.status>=200 && xhr.status<=299) {
-       let data  = JSON.parse(xhr.response);
-       console.log(data);
-       title.value = data.title;
-       body.value = data.body;
-       userId.value = data.userId;
-       sBtn.classList.add('d-none');
-       uBtn.classList.remove('d-none');
-      }
-  }
-}
-
-
-const temp = (arr) => {
-  let result = "";
-  arr.forEach((add) => {
-    result += `
-           <div class="col-md-4">
-          <div class="card mb-3" id ='${add.id}'>
-            <div class="card-header">
-              <h3>${add.title}</h3>
-            </div>
-            <div class="card-body">
-                  <p> ${add.body}</p>
-            </div>
+const createCard = (obj,data)=>{
+    let card = document.createElement('div')
+    card.className = 'col-md-4 offset-md-4';
+    card.id = data.name;
+    card.innerHTML = `
+            <div class = 'card'>
+            <div class="card-header">${obj.title}</div>
+            <div class="card-body">${obj.body}</div>
             <div class="card-footer d-flex justify-content-between">
-              <button class="btn btn-primary" onclick = "onEdit(this)">Edit</button>
-              <button class="btn btn-danger"  onclick = "onDelete(this)">Delete</button>
+            <button class="btn btn-primary" onclick='onEdit(this)'>Edit</button>
+            <button class="btn btn-danger" onclick='onDelete(this)'>Delete</button>
             </div>
-          </div>
-         </div>
-        
+            </div>
+    
+    `
+        dataContainer.append(card)
+
+}
+
+
+
+const fetchAllDataFromDB = ()=>{
+    loader.classList.remove('d-none')
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',Post_Url,true)
+    xhr.send(null)
+    xhr.onload = ()=>{
+        if(xhr.status>=200 && xhr.status<=299)
+        {
+            let data = JSON.parse(xhr.response);
+            cl(data)
+            let postArr = objToarr(data)
+            temp(postArr)
+            // cl(postArr)
+            // const formattedArray = Object.entries(data).map(([key, value]) => ({ id: key, ...value }));
+            // console.log(formattedArray);
+
+
+        }
+
+        loader.classList.add('d-none')
+    }
+}
+
+fetchAllDataFromDB()
+
+
+
+
+
+const temp = (arr=>{
+    let result = '';
+    arr.forEach(add => {
+        result += `
+        <div class='col-md-4 offset-md-4'>     
+            <div class="card mb-3 my-3">
+                <div class="card-header">${add.title}</div>
+                <div class="card-body">${add.body}</div>
+                <div class="card-footer d-flex justify-content-between">
+                    <button class="btn btn-primary" onclick='onEdit(this)'>Edit</button>
+                    <button class="btn btn-danger" onclick='onDelete(this)'>Delete</button>
+                </div>
+            </div>
+        </div>
         `;
-    cardsContainer.innerHTML = result;
-  });
-};
+    });
+    dataContainer.innerHTML = result;
+})
+userData.addEventListener('submit',onsendObjToDB)
 
 
-
-
-const addDataTosaveInDB = (eve) => {
-  eve.preventDefault();
-  let newObj = {
-    title: title.value,
-    body: body.value,
-    userId: userId.value,
-  };
-
-  userForm.reset()
-  cl(newObj);
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", Post_Url, true);
-  xhr.send(JSON.stringify(newObj));
-  xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status <= 299) {
-      let data = JSON.parse(xhr.response);
-      cl(data);
-      let card = document.createElement("div");
-      card.className = "col-md-4";
-      card.id = data.id;
-      card.innerHTML = `
-           <div class ="card text-center" id="${data.id}">
-           <div class="card-header">
-              <h3>${newObj.title}</h3>
-            </div>
-            <div class="card-body">
-                   <p> ${newObj.body}</p>
-            </div>
-            <div class="card-footer d-flex justify-content-between">
-              <button class="btn btn-primary"   onclick = "onEdit(this)">Edit</button>
-              <button class="btn btn-danger"    onclick=  "onDelete(this)">Delete</button>
-            </div>
-            </div>
-          
-            
-            `;
-      cardsContainer.append(card);
-      snackBar('Post Added SucsessFully','success')
-    } 
-
-    else
-    {
-       snackBar('somthing went wrong','error')
-    }
-  };
-};
-
-
-
-
-
-const getDatathroughAPI = () => {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", Post_Url, true);
-  xhr.send(null);
-  xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status <= 299) {
-      let data = JSON.parse(xhr.response);
-      cl(data);
-      temp(data);
-    }
-  };
-};
-
-getDatathroughAPI();
-
-userForm.addEventListener("submit", addDataTosaveInDB);
-uBtn.addEventListener('click',onUpdate)
